9cddd19fe82394149d350ef16de276cd58ca087f
Risav Sarkar <risavabir2001@gmail.com>
2021-09-24 19:02:05 +0530

    Setup Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./styles/styles.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home";
+import Movie from "./components/movie";
 import Footer from "./components/footer";
 
-function App() {
-  return (
-    <>
-      <Home />
-      <Footer />
-    </>
-  );
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:movieId" element={<Movie />} />
+    </Routes>
+    <Footer />
+  </Router>
+);
 
 export default App;
