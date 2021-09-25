6838f2c0857611144afebafac11b2a81779b9eda
Risav Sarkar <risavabir2001@gmail.com>
2021-09-25 15:40:48 +0530

    Update movie.js

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,11 +1,15 @@
+// eslint-disable-next-line
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 import { useParams } from "react-router";
+// eslint-disable-next-line
 import Spinner from "./spinner";
+// eslint-disable-next-line
 import Grid from "./grid";
 import { useMovieFetch } from "../hooks/useMovieFetch";
 
 const Movie = () => {
   const { movieId } = useParams();
+  // eslint-disable-next-line
   const { state, loading, error } = useMovieFetch(movieId);
   console.log(state);
   return <div>Movie</div>;
