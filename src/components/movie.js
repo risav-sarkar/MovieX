ec22154f8a2cead63e6e51dc3f628d2d880987cf
Risav Sarkar <risavabir2001@gmail.com>
2021-09-26 10:05:29 +0530

    Created Movie component

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,18 +1,45 @@
-// eslint-disable-next-line
-import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from "../config";
 import { useParams } from "react-router";
-// eslint-disable-next-line
 import Spinner from "./spinner";
-// eslint-disable-next-line
-import Grid from "./grid";
+import HeroImage from "./homeComponents/heroImage";
 import { useMovieFetch } from "../hooks/useMovieFetch";
 
 const Movie = () => {
   const { movieId } = useParams();
-  // eslint-disable-next-line
   const { state, loading, error } = useMovieFetch(movieId);
   console.log(state);
-  return <div>Movie</div>;
+
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>;
+
+  if (Object.keys(state).length) {
+    return (
+      <div className="movie">
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.backdrop_path}`}
+        />
+        <div className="movieDetailsContainer">
+          <div className="blur"></div>
+          <div className="movieDetails">
+            <h1>{state.original_title}</h1>
+            <h3>"{state.tagline}"</h3>
+            <div className="movieSpecs">
+              <h4>{state.release_date.substring(0, 4)}</h4>
+              <div className="bar"></div>
+              <h4>{`${Math.floor(state.runtime / 60)}h ${
+                state.runtime % 60
+              }m`}</h4>
+              <div className="bar"></div>
+              {state.genres.map((e) => (
+                <h4 key={e.id}>{e.name}</h4>
+              ))}
+            </div>
+            <p>{state.overview}</p>
+          </div>
+        </div>
+      </div>
+    );
+  } else return null;
 };
 
 export default Movie;
